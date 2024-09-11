3a9fecd545f3bcac1a606d7f068da26a5046f4ae
Andrea Nocciolini <andreanocciolini93@gmail.com>
2024-09-11 17:22:55 +0200

    add documents type sinclair/typebox

--- src/types/documents.ts
+++ src/types/documents.ts
@@ -1,1 +1,17 @@
-export type DocumentsType = "air-waybill" | "cn23" | "cn23-default" | "commercial-invoice" | "cp71" | "label" | "qr"
+import { Type, Static } from "@sinclair/typebox";
+
+const Documents = Type.Union([
+    Type.Literal("air-waybill"),
+    Type.Literal("cn23"),
+    Type.Literal("cn23-default"),
+    Type.Literal("commercial-invoice"),
+    Type.Literal("cp71"),
+    Type.Literal("label"),
+    Type.Literal("qr"),
+]);
+
+type DocumentsType = Static<typeof Documents>;
+
+export {
+    DocumentsType
+}
